36e0038e517dbe69dbd6d9abb4345a4465866f67
Jordan <jordan.benson4@gmail.com>
2022-01-11 04:52:41 -0800

    Changed linebreak format from CRLF to LF

--- assertEqual.js
+++ assertEqual.js
@@ -3,9 +3,9 @@ const assertEqual = function(actual, expected) {
   const pass = '✅';
   const fail = '🛑';
   if (actual === expected) {
-    console.log(`${pass}${pass}${pass} Assertion Passed: ${actual} === ${expected}`);
+    console.log(`${pass} Assertion Passed: ${actual} === ${expected}`);
   } else {
-    console.log(`${fail}${fail}${fail} Assertion Failed: ${actual} !== ${expected}`);
+    console.log(`${fail} Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 // TEST CODE
