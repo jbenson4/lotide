a89f8b7074c45d28240076c946cd328eae15d143
Jordan <jordan.benson4@gmail.com>
2022-01-12 01:31:14 -0800

    Added assertArraysEqual.js and eqArrays.js

---
+++ assertArraysEqual.js
@@ -0,0 +1,22 @@
+const assertArraysEqual = function(array1, array2) {
+  const pass = '✅';
+  const fail = '🛑';
+  if (array1.length !== array2.length) {
+    console.log(`${fail} Assertion Failed: [${array1}] !== [${array2}]`);
+  }
+  let isTrue = true;
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      isTrue = false;
+    }
+  }
+  if (isTrue === true) {
+    console.log(`${pass} Assertion Passed: [${array1}] === [${array2}]`);
+  }
+  if (isTrue === false) {
+    // console.log(fail + ' Assertion Failed: ' + array1 + ' !== ' + array2)
+    console.log(`${fail} Assertion Failed: [${array1}] !== [${array2}]`);
+  }
+};
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
+assertArraysEqual([1, 2, 3], [3, 2, 1]);
---
+++ eqArrays.js
@@ -0,0 +1,28 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  const pass = '✅';
+  const fail = '🛑';
+  if (actual === expected) {
+    console.log(`${pass} Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`${fail} Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  let isTrue = true;
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      isTrue = false;
+    }
+  }
+  return isTrue;
+};
+// TEST CODE
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+assertEqual(eqArrays(['1', '2', '3'], ['1', '2', '3']), true);
+assertEqual(eqArrays(['1', '2', '3'], ['1', '2', 3]), false);
