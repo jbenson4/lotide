8806080bab509b502919dfd5d1e9a2d86a31bc31
Jordan <jordan.benson4@gmail.com>
2022-01-12 23:11:41 -0800

    Adding finished countOnly.js

--- countOnly.js
+++ countOnly.js
@@ -9,8 +9,19 @@ const assertEqual = function(actual, expected) {
 };
 
 const countOnly = function(allItems, itemsToCount) {
-
-}
+  const results = {};
+  for (const item of allItems) {
+    if (itemsToCount[item]) {
+      if (results[item]) {
+        results[item] += 1;
+      } else {
+        results[item] = 1;
+      }
+    }
+  }
+  console.log(results);
+  return results;
+};
 
 const firstNames = [
   "Karl",
