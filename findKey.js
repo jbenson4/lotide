d3dab42b2ff318878213188c71319d2776391e13
Jordan <jordan.benson4@gmail.com>
2022-01-13 23:45:56 -0800

    Added findKey.js

---
+++ findKey.js
@@ -0,0 +1,35 @@
+const assertEqual = function(actual, expected) {
+  const pass = '✅';
+  const fail = '🛑';
+  if (actual === expected) {
+    console.log(`${pass} Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`${fail} Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKey = function(object, callback) {
+  for (let key in object) {
+    // console.log('key: ', key);
+    // console.log('object[key]: ', object[key]);
+    if (callback(object[key])) return key;
+  }
+};
+const result1 = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2);
+
+const result2 = findKey({
+  Mclaren: { model: 'P1'},
+  Ferrari: { model: 488},
+  Lamborghini: { model: 'Huracan'},
+  Koenigsegg: { model: 'Jasko'}
+}, x => x.model === 'Jasko');
+
+assertEqual(result1, 'noma');
+assertEqual(result2, 'Koenigsegg');
